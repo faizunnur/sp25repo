5c65e0f2993f98c253858af0a1e95223f2d9d9b7
faizunnur <faizunnur71@gmail.com>
2026-03-25 22:40:54 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/test/java/com/example/sp25repo/Sp25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sp25repo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sp25ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
