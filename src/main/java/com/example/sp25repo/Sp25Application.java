54c6166ce75f541594045c9de5a8e97d78c5ce0a
faizunnur <faizunnur71@gmail.com>
2026-03-25 22:40:59 +0600

    chore: bootstrap Spring Boot starter from Spring Initializr

---
+++ src/main/java/com/example/sp25repo/Sp25Application.java
@@ -0,0 +1,13 @@
+package com.example.sp25repo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp25Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp25Application.class, args);
+	}
+
+}
